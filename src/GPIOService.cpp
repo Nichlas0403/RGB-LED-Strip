66c106396d733ea8cffbba8cc771c276faf2063b
Nichlas0403 <nichlas0403@gmail.com>
2023-11-09 19:17:16 +0100

    Removed date and photoresistor responsibility from project

---
+++ src/GPIOService.cpp
@@ -0,0 +1,16 @@
+#include "GPIOService.h"
+GPIOService::GPIOService(int relayGPIO)
+{
+    _relayGPIO = relayGPIO;
+    pinMode(_relayGPIO, OUTPUT);
+}
+void GPIOService::TurnRelayOff()
+{
+    digitalWrite(_relayGPIO, LOW);
+    RelayState = false;
+}
+void GPIOService::TurnRelayOn()
+{
+    digitalWrite(_relayGPIO, HIGH);
+    RelayState = true;
+}
